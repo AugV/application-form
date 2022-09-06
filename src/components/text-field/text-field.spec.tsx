8ed23d845f96fdd2a62e01f6e88ebac37d742979
augustinas <augustinas.vainius@gmail.com>
2022-09-06 19:24:01 +0300

    testing-library/user-event installed & change event test for text field

--- src/components/text-field/text-field.spec.tsx
+++ src/components/text-field/text-field.spec.tsx
@@ -1,28 +1,52 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import { TextField } from "./text-field";
+import userEvent from "@testing-library/user-event";
 
 const defaultProps = {
   label: "default-label",
 };
 
-const factory = (props?: React.ComponentProps<typeof TextField>) =>
+const factory = (props?: Partial<React.ComponentProps<typeof TextField>>) =>
   render(<TextField {...defaultProps} {...props} />);
 
-/* A topic of disscussion if such tests are needed since it's just a wrapper for component from component library
+/* A topic of disscussion if such tests are needed since it's just a wrapper for component from component library.
     However if we do have such tests, 
     1) we can be more confident about our integration with the consumed components, especially during version bumps
     2) in case we'd need to switch out the library, we know we're not losing any functionality or introducing bugs.
 */
 
-test("should render input element", () => {
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+test("renders input element", () => {
   factory();
 
   expect(screen.getByRole("textbox")).toBeVisible();
 });
 
-test("should have label", () => {
+test("renders provided text value", () => {
+  factory({ value: "test-value" });
+
+  expect(screen.getByDisplayValue("test-value")).toBeVisible();
+});
+
+test("has label", () => {
   factory({ label: "test-label" });
 
   expect(screen.getByLabelText("test-label")).toBeVisible();
+});
+
+test("fires change callback when value is edited", async () => {
+  const user = userEvent.setup();
+  const mockCallback = jest.fn();
+  factory({ label: "test-label", onChange: mockCallback });
+
+  const inputField = screen.getByLabelText("test-label");
+  await user.type(inputField, "new-test-value");
+
+  expect(mockCallback).toHaveBeenLastCalledWith(
+    expect.objectContaining({ target: expect.objectContaining({ value: "new-test-value" }) })
+  );
 });
--- src/components/text-field/text-field.tsx
+++ src/components/text-field/text-field.tsx
@@ -1,9 +1,19 @@
 import { TextField as MuiTextField } from "@mui/material";
+import { ChangeEvent } from "react";
 
 type TextFieldProps = {
-    label: string;
+  label: string;
+  value?: string;
+  onChange: (event: ChangeEvent) => void; 
 };
 
-export const TextField = ({ label }: TextFieldProps) => {
-  return <MuiTextField label={label} />;
+/* Why not use MuiTextField directly? 
+    We're doing this to decouple MUI from our app. 
+    Why decouple? 
+    Because 
+    1) if there would be problems/breakingchanges with MuiTextField it would be centralised 
+    2) easier to migrate 
+*/
+export const TextField = ({ label, value, onChange }: TextFieldProps) => {
+  return <MuiTextField label={label} value={value} onChange={onChange} />;
 };
